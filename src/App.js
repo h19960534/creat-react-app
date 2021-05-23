d8cd8a515edb62bc5e3e1af07c82f2c79bdc1274
blue_raincoat <h19960534@gmail.com>
2021-05-24 00:14:23 +0800

    antd use

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react'
-import {Route,Redirect,Switch} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import MyNavLink from './components/MyNavLink'
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
 
 export default class App extends Component {
-    render() {           
+    render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col-xs-offset-2 col-xs-8">
-                        <div className="page-header"><h2>React Router Demo</h2></div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
-                            {/* <a className="list-group-item" href="./about.html">About</a>
-                            <a className="list-group-item active" href="./home.html">Home</a> */}                
-                                <MyNavLink to="/about">About</MyNavLink>
-                                <MyNavLink to="/home">Home</MyNavLink>
-                        </div>
-                    </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-                            <Switch>
-                                <Route path="/about" component={About}/>
-                                <Route path="/home" component={Home}/> 
-                                <Redirect to="/about"/>
-                            </Switch>                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Button type="primary">点我</Button>
             </div>
         )
     }
